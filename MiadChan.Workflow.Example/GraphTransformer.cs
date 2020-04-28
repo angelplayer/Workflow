35607d810761147c2e1b3f3354bbdb3eee0ba573
Chamsamnang EM <chamsamnang.em@presta.tessi.fr>
2020-04-28 16:13:49 +0700

    Custom attribute for graph workflow metadata generation

--- MiadChan.Workflow.Example/GraphTransformer.cs
+++ MiadChan.Workflow.Example/GraphTransformer.cs
@@ -2,29 +2,33 @@
 using System.Text;
 using System.Text.Json;
 using System.Collections.Generic;
-using System.Buffers;
 using System.IO;
+using System;
+using Maidchan.Workflow.Attributes;
+using System.Reflection;
 
 namespace MiadChan.Workflow.Example
 {
-    public struct Edge {
+    public struct Edge
+    {
         public string From;
         public string To;
     }
 
     public class GraphTransformer
     {
-        public static string ConvertToDegreD3Object(string workflow) 
+        public static string ConvertToDegreD3Object(string workflow)
         {
             var options = new JsonWriterOptions()
             {
                 Indented = true
             };
 
-            
+
             var output = new MemoryStream();
-            using (var writer = new Utf8JsonWriter(output, options)) {
-                
+            using (var writer = new Utf8JsonWriter(output, options))
+            {
+
                 JsonDocument document = JsonDocument.Parse(workflow);
                 List<Edge> edges = new List<Edge>();
 
@@ -32,7 +36,7 @@ namespace MiadChan.Workflow.Example
                 int version = document.RootElement.GetProperty("Version").GetInt16();
 
                 writer.WriteStartObject();
-                
+
                 writer.WritePropertyName("Id");
                 writer.WriteStringValue(id);
 
@@ -42,47 +46,97 @@ namespace MiadChan.Workflow.Example
                 writer.WritePropertyName("Nodes");
                 writer.WriteStartArray();
 
-                    var steps = document.RootElement.GetProperty("Steps").EnumerateArray();
-                    foreach(var step in steps) {
-                        writer.WriteStartObject();
+                var steps = document.RootElement.GetProperty("Steps").EnumerateArray();
+                foreach (var step in steps)
+                {
+                    writer.WriteStartObject();
 
-                        writer.WritePropertyName("Id");
-                        var stepId = step.GetProperty("Id").GetString();
-                        writer.WriteStringValue(stepId);
+                    writer.WritePropertyName("Id");
+                    var stepId = step.GetProperty("Id").GetString();
+                    writer.WriteStringValue(stepId);
 
-                        writer.WritePropertyName("kind");
-                        writer.WriteStringValue("task");
+                    writer.WritePropertyName("kind");
+                    writer.WriteStringValue("task");
 
-                        writer.WritePropertyName("Props");
-                        writer.WriteStartObject();
-                        writer.WriteEndObject();
+                    var stepType = step.GetProperty("StepType").GetString();
+                    WriteStepTypeParams(writer, stepType);
 
-                        if(step.TryGetProperty("NextStepId", out var to)) {
-                            edges.Add(new Edge {From = stepId, To = to.GetString()});
-                        }
+                    if (step.TryGetProperty("NextStepId", out var to))
+                    {
+                        edges.Add(new Edge { From = stepId, To = to.GetString() });
+                    }
 
                     writer.WriteEndObject();
-                    }    
+                }
 
                 writer.WriteEndArray();
 
                 writer.WritePropertyName("edges");
                 writer.WriteStartArray();
-                    foreach(var edge in edges) {
-                        writer.WriteStartObject();
-                        writer.WritePropertyName("from");
-                        writer.WriteStringValue(edge.From);
-                        writer.WritePropertyName("to");
-                        writer.WriteStringValue(edge.To);
-                        writer.WriteEndObject();
-                    }
+                foreach (var edge in edges)
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName("from");
+                    writer.WriteStringValue(edge.From);
+                    writer.WritePropertyName("to");
+                    writer.WriteStringValue(edge.To);
+                    writer.WriteEndObject();
+                }
                 writer.WriteEndArray();
 
                 writer.WriteEndObject();
                 writer.Flush();
             }
-            
+
             return Encoding.UTF8.GetString(output.GetBuffer());
+        }
+
+        public static void WriteStepTypeParams(Utf8JsonWriter writer, string stepType)
+        {
+
+            Type type = Type.GetType(stepType);
+            if (type == null)
+            {
+                throw new Exception($"Unreconigzed task {stepType}");
+            }
+
+            var stepAttr = type.GetCustomAttribute<StepTypeAttribute>();
+            if (stepAttr != null)
+            {
+                if (!string.IsNullOrEmpty(stepAttr.Description))
+                {
+                    writer.WritePropertyName("description");
+                    writer.WriteStringValue(stepAttr.Description);
+                }
+            }
+
+            var propInfos = type.GetProperties();
+            writer.WritePropertyName("Props");
+            writer.WriteStartArray();
+
+            foreach (var item in propInfos)
+            {
+                var inputAttr = item.GetCustomAttribute<InputAttribute>();
+                if (inputAttr != null)
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName("name");
+                    writer.WriteStringValue(item.Name);
+
+                    writer.WritePropertyName("datatype");
+                    writer.WriteStringValue(inputAttr.Kind.ToString());
+
+                    if (!string.IsNullOrEmpty(inputAttr.HelpText))
+                    {
+                        writer.WritePropertyName("help");
+                        writer.WriteStringValue(inputAttr.HelpText);
+                    }
+                    writer.WriteEndObject();
+                }
+            }
+
+            writer.WriteEndArray();
+
         }
     }
 }

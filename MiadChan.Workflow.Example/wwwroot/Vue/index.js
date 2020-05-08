2d3ab8faf9273cdee2214e50b366008d75e2b0fe
Chamsamnang EM <chamsamnang.em@presta.tessi.fr>
2020-05-08 11:27:51 +0700

    Visjs network with Vuejs , add, connect and delete

--- MiadChan.Workflow.Example/wwwroot/Vue/index.js
+++ MiadChan.Workflow.Example/wwwroot/Vue/index.js
@@ -6,7 +6,7 @@
         props: ['action', 'index'],
         template: `
             <li class="action-item">
-                <button @click="buttonClick">{{action.stepType}}</button>
+                <button @click="buttonClick">{{action}}</button>
             </li>
         `,
 
@@ -17,14 +17,28 @@
         }
     });
 
-    Vue.component('ActionList',  {
+    Vue.component('ActionList', {
         props: ['items'],
+        data: function () {
+            return {
+                defaultAction: [
+                    { action: 'Connect' },
+                    { action: 'Decision'}
+                ]
+            }
+        },
         template: `
             <ul class="action-list">
+                <action-item v-for="(item, index) in defaultAction"
+                    v-bind:key="item+index"
+                    v-bind:action="item.action"
+                    v-bind:index="index"
+                    v-on:selected="onOtherSelected"
+                />
                 <action-item 
                     v-for="(item, index) in items" 
                     v-bind:key="item.stepType" 
-                    v-bind:action="item"
+                    v-bind:action="item.stepType"
                     v-bind:index="index"
                     v-on:selected="onSelected"
                 />
@@ -32,11 +46,13 @@
         `,
         methods: {
             onSelected: function (selectedIndex) {
-                this.$emit('change', selectedIndex);
+                this.$emit('change', selectedIndex, "stepType");
+            },
+            onOtherSelected: function (selectedIndex) {
+                this.$emit('change', selectedIndex, this.defaultAction[selectedIndex].action);
             }
         }
     });
-
 
     const vue = new Vue({
         el: '#editor',
@@ -45,7 +61,10 @@
 
                 text: 'hello',
                 steplist: [],
-                currentSelectedIndex: -1
+                currentSelection: {
+                    index: -1, kind: ''
+                },
+                connectEventCallback: []
             }
         },
 
@@ -61,41 +80,154 @@
         },
 
         methods: {
-            onchange: function(selectedIndex) {
-                this.$set(this, 'currentSelectedIndex', selectedIndex);
+            onchange: function (selectedIndex, actionKind) {
+                if (actionKind === 'Connect') {
+                    this.resetIndex();
+
+                    // Publish event Connect
+                    Array.from(this.connectEventCallback).forEach(cb => cb());
+                    return;
+                }
+
+                this.$set(this, 'currentSelection', {index: selectedIndex, kind: actionKind});
+            },
+
+            onConnectEvent: function (callback) {
+                this.connectEventCallback.push(callback);
             },
 
             resetIndex: function () {
-                this.$set(this, 'currentSelectedIndex', -1);
+                this.$set(this, 'currentSelection', {index: -1, kind: ''});
             }
         },
 
         computed: {
             selectAction: function () {
-                return this.currentSelectedIndex > -1 ? this.steplist[this.currentSelectedIndex] : '';
+                return this.currentSelection.kind;
+            },
+            getAction: function () {
+                return this.currentSelection.index > -1 ? this.steplist[this.currentSelection.index] : {} ;
+                
             }
+
         }
     });
 
-    let container = document.getElementById('workflow');
-    let options = {}
-    const data = {
-        nodes: new vis.DataSet([]),
-        edges: new vis.DataSet([])
-    }
-    let network = new vis.Network(container, data, options);
 
-    network.on('click', () => {
-        if (vue.selectAction !== '') {
-            try {
-                let newNode = {shape: 'box', id: 1, label: vue.selectAction.stepType};
-                data.nodes.add(newNode);
-            } catch (err) {
-                console.log(err);
+    const workflow = {
+        init: (settings) => {
+            let container = document.getElementById(settings.el);
+            
+            let options = {
+                interaction: {
+                    hover: true,
+                    hoverConnectedEdges: false,
+                    selectConnectedEdges: false,
+                },
+                manipulation: {
+                    initiallyActive: false,
+                    enabled: false,
+                    addNode: false,
+                    addEdge: true,
+                    deleteNode: false,
+                    deleteEdge: false,
+                },
+                physics: {
+                    enabled: false
+                },
+                edges: {
+                    arrows: {
+                        to: { type: 'triangle', enabled: true }
+                    },
+                    color: '#242020',
+                    smooth: {
+                        type: 'horizontal',
+                        roundness: 0
+                    },
+                },
+                nodes: {
+                    shape: 'box',
+                    margin: {
+                        top: 15, bottom: 15, right: 10, left: 10
+                    },
+                    borderWidth: 0.3,
+                    borderWidthSelected: 1,
+                    color: {
+                        background: '#5bb8ab',
+                        hover: {
+                            background: "#95c7c0",
+                        }
+                    }
+                }
             }
-        } 
 
-        vue.resetIndex();
+            const data = {
+                nodes: new vis.DataSet([]),
+                edges: new vis.DataSet([])
+            }
+            let network = new vis.Network(container, data, options);
+
+
+            // Methods
+            const addNode = (newNode) => {
+                try {
+                    data.nodes.add(newNode);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+
+
+            // Events
+            vue.onConnectEvent(() => {
+                if (network.manipulation.inMode === 'addEdge') {
+                    network.disableEditMode();
+                } else {
+                    network.addEdgeMode();
+                }
+            });
+
+            container.addEventListener('keyup', (evt) => {
+                if (evt.key === 'Delete') {
+                    network.deleteSelected();
+                };
+            });
+
+            network.on('selectNode', (mouse) => {
+
+            });
+
+            network.on('selectEdge', (mouse) => {
+                
+            });
+
+            network.on('click', (mouse) => {
+                if (vue.selectAction !== '') {
+                    let node = { x: mouse.pointer.canvas.x, y: mouse.pointer.canvas.y }
+
+                    let kind = vue.selectAction;
+                    if (kind === 'stepType') {
+                        node.shape = 'box';
+                        node.label = vue.getAction.stepType;
+                        addNode(node);
+                    } else if (kind === 'Decision') {
+                        node.shape = 'diamond';
+                        node.label = kind;
+                        addNode(node);
+                    } else {
+                        // Ignore other type
+                    }
+                    
+                };
+
+            network.on('')
+
+                vue.resetIndex();
+            });
+        },
+    }
+
+    workflow.init({
+        el: 'workflow'
     });
-
 })();

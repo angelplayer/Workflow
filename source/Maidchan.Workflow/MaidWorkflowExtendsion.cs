a648b210c6ed63e64c27209e240600772e94eb0d
angelplayer <angel.player@outlook.com>
2020-07-29 15:51:49 +0700

    Update MaidWorkflowExtendsion.cs

--- source/Maidchan.Workflow/MaidWorkflowExtendsion.cs
+++ source/Maidchan.Workflow/MaidWorkflowExtendsion.cs
@@ -1,3 +1,4 @@
+using System;
 using Maidchan.Workflow.Storages;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,12 +6,38 @@ using WorkflowCore.Interface;
 
 namespace Maidchan.Workflow
 {
+  public class WorkflowOption {
+    public string ConnectionString { get; set; }
+    public ProviderType Provider { get; set; } = ProviderType.DEFAULT;
+    public bool CanCreateDb { get; set; }
+
+    public enum ProviderType {
+      DEFAULT = 0,
+      SQLITE = 1,
+      PGSQL = 2
+    }
+  }
+
   public static class MaidWorkflowExtendsion
   {
-    public static IServiceCollection AddMaidWorkflow(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddMaidWorkflow(this IServiceCollection serviceCollection, Action<WorkflowOption> config = null)
     {
+      var theOption = new WorkflowOption();
+      config?.Invoke(theOption);
+
+      if(theOption.Provider == WorkflowOption.ProviderType.SQLITE) 
+      {
+        serviceCollection.AddWorkflow(x => x.UseSqlite(theOption.ConnectionString, theOption.CanCreateDb));
+      } else if(theOption.Provider == WorkflowOption.ProviderType.PGSQL) 
+      {
+        // TODO: look for PGSLQ support
+        serviceCollection.AddWorkflow();
+      } else {
+        serviceCollection.AddWorkflow();
+      }
+
       serviceCollection.AddTransient<IGraphStore, GraphStore>();
-      serviceCollection.AddWorkflow(/*x => x.UseSqlite(@"Data Source=database.db;", true)*/);
+      
       serviceCollection.AddWorkflowDSL();
       serviceCollection.AddTransient<IWorkflowManager, WorkflowManager>();
       serviceCollection.AddTransient<IGraphConnector, GraphConnector>();
